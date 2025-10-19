90e0a4c2638e59b0f5f3d19da9a61fff036cbf28
KennyKim33 <kennykimm12@gmail.com>
2025-10-20 02:25:42 +0900

    feat: 구분자를 기준으로 분리하는 DelimiterParser 클래스 구현 - 기본 구분자와 커스텀 구분자 지원 - 커스텀 구분자 형식에 대한 검증 구현 - 정규식 표현에 대한 Pattern.quote 사용

---
+++ src/main/java/calculator/service/DelimiterParser.java
@@ -0,0 +1,66 @@
+package calculator.service;
+
+import java.util.regex.Pattern;
+
+public class DelimiterParser {
+    private static final String DEFAULT_DELIMITER = "[:;]";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\n";
+
+    public String parse(String input) {
+        String delimiter = getDelimiter(input);
+        String expressionPart = getExpression(input);
+        return  String.join("", expressionPart.split(delimiter));
+    }
+
+    private String getExpression(String input) {
+        if(hasCustomDelimiter(input)) {
+            int delimiterEndIndex = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
+            return input.substring(delimiterEndIndex + 1);
+        }
+        return input;
+    }
+
+    private boolean hasCustomDelimiter(String input) {
+        return input.startsWith(CUSTOM_DELIMITER_PREFIX);
+    }
+
+    private String getDelimiter(String input) {
+        if(hasCustomDelimiter(input)) {
+            String customDelimiter = extractCustomDelimiter(input);
+            return buildDelimiterPattern(customDelimiter);
+        }
+        return DEFAULT_DELIMITER;
+    }
+
+    private String extractCustomDelimiter(String input) {
+        int delimiterEndIndex = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
+
+        validateCustomDelimiterFormat(delimiterEndIndex);
+
+        return input.substring(2, delimiterEndIndex);
+    }
+
+    private String buildDelimiterPattern(String customDelimiter) {
+        StringBuilder sb = new StringBuilder(DEFAULT_DELIMITER);
+
+        validateDelimiterCount(customDelimiter);
+
+        sb.insert(1, Pattern.quote(customDelimiter));
+
+        return sb.toString();
+
+    }
+
+    private void validateCustomDelimiterFormat(int delimiterEndIndex) {
+        if(delimiterEndIndex == -1) {
+            throw new IllegalArgumentException("커스텀 구분자 지원 형식을 지켜주세요");
+        }
+    }
+
+    private void validateDelimiterCount(String customDelimiter) {
+        if(customDelimiter.length() != 1) {
+            throw new IllegalArgumentException("커스텀 구분자는 1개의 문자만 가능합니다.");
+        }
+    }
+}
---
+++ src/test/java/calculator/service/DelimiterParserTest.java
@@ -0,0 +1,71 @@
+package calculator.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class DelimiterParserTest {
+    private DelimiterParser delimiterParser;
+
+    @BeforeEach
+    void setUp() {
+        delimiterParser = new DelimiterParser();
+    }
+
+    @Test
+    void 정상_기본_구분자_파싱_테스트() {
+        String input = "1:2;3";
+
+        String parsed = delimiterParser.parse(input);
+
+        assertThat(parsed).isEqualTo("123");
+    }
+
+    @Test
+    void 정상_커스텀_구분자_파싱_테스트() {
+        String input = "//+\n1+2:3";
+
+        String parsed = delimiterParser.parse(input);
+
+        assertThat(parsed).isEqualTo("123");
+    }
+
+    @Test
+    void 커스텀_구분자_형식_오류() {
+        String input = "//+1+2+3";
+
+        assertThatThrownBy(() -> delimiterParser.parse(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("커스텀 구분자 지원 형식을 지켜주세요");
+    }
+
+    @Test
+    void 커스텀_구분자_개수_오류_2개_이상() {
+        String input = "//++\n1++2++3";
+
+        assertThatThrownBy(() -> delimiterParser.parse(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("커스텀 구분자는 1개의 문자만 가능합니다.");
+    }
+
+    @Test
+    void 커스텀_구분자_개수_오류_0개() {
+        String input = "//\n1:2:3";
+
+        assertThatThrownBy(() -> delimiterParser.parse(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("커스텀 구분자는 1개의 문자만 가능합니다.");
+    }
+
+
+    @Test
+    void 커스텀_구분자와_기본_구분자_혼용() {
+        String input = "//!\n1!2:3;4";
+
+        String parsed = delimiterParser.parse(input);
+
+        assertThat(parsed).isEqualTo("1234");
+    }
+}
