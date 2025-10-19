a1600d3e8fc0dabbd80e96fc7e957858dbdc745c
KennyKim33 <kennykimm12@gmail.com>
2025-10-19 22:37:48 +0900

    feat: 계산 결과 출력을 위한 OutputView 구현 - `showResult(int result)` 정적 메서드를 통해 결과를 포맷에 맞게 출력 - 콘솔 출력을 담당하여 사용자에게 최종 결과를 표시하는 역할

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class OutputView {
+    private OutputView() {}
+
+    public static void showResult(int result){
+        System.out.printf("결과 : %d",result);
+    }
+}
